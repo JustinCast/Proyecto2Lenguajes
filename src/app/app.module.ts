f90a82cf7b3d2bec751ea85b9d27ad0dc674eab1
Justin Castillo Valladares <justincastillovalladares@gmail.com>
2018-09-15 22:54:46 -0600

    feat: working on initials

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,24 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { AppComponent } from "./app.component";
-import { AhorcadoGameComponent } from './ahorcado-game/ahorcado-game.component';
-import { SopaLetrasGameComponent } from './sopa-letras-game/sopa-letras-game.component';
+import { RouterModule } from "@angular/router";
+import { SharedModule } from "./shared/shared.module";
+import { ROUTES } from "./app.routing";
+import { AhorcadoGameComponent } from "./ahorcado-game/ahorcado-game.component";
+import { SopaLetrasGameComponent } from "./sopa-letras-game/sopa-letras-game.component";
 
 @NgModule({
-  declarations: [AppComponent, AhorcadoGameComponent, SopaLetrasGameComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  declarations: [
+    AppComponent,
+    AhorcadoGameComponent,
+    SopaLetrasGameComponent
+  ],
+  imports: [
+    BrowserModule, 
+    BrowserAnimationsModule,
+    SharedModule,
+    RouterModule.forRoot(ROUTES)
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
