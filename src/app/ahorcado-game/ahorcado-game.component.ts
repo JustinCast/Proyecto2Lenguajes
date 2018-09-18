1ad8fa9cd822c3dd4853aee42902473cae6aea1b
Justin Castillo Valladares <justincastillovalladares@gmail.com>
2018-09-17 18:54:00 -0600

    feat: Creación de input

--- src/app/ahorcado-game/ahorcado-game.component.ts
+++ src/app/ahorcado-game/ahorcado-game.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild } from "@angular/core";
 import * as data from "./ahorcadoGameWords.json";
 import { PageEvent } from "@angular/material";
+import { FormGroup, FormBuilder } from "@angular/forms";
 @Component({
   selector: "app-ahorcado-game",
   templateUrl: "./ahorcado-game.component.html",
@@ -8,21 +9,25 @@ import { PageEvent } from "@angular/material";
 })
 export class AhorcadoGameComponent implements OnInit {
   words: Array<string> = (<any>data).words;
+  hiddenWord: string;
   selectedWord: string;
+  hangedFG: FormGroup;
+
   length = this.words.length;
   pageSize = 4;
   pageSizeOptions: number[] = [4, 5];
-
   // MatPaginator Output
   pageEvent: PageEvent;
   activePageDataChunk = [];
 
-  constructor() {}
+  constructor(private _fb: FormBuilder) {
+    this.hangedFG = this._fb.group({
+      'word': ['']
+    });
+  }
 
   ngOnInit() {
-    console.log(this.words);
     this.activePageDataChunk = this.words.slice(0, this.pageSize);
-    console.log(this.activePageDataChunk);
   }
 
   setPageSizeOptions(setPageSizeOptionsInput: string) {
@@ -30,11 +35,11 @@ export class AhorcadoGameComponent implements OnInit {
   }
 
   genRandom() {
-    this.selectedWord = '';
+    this.hiddenWord = '';
     let randomIndex = Math.floor(Math.random() * (this.words.length - 1)) + 1;
-    console.log(this.words[randomIndex]);
+    this.selectedWord = this.words[randomIndex];
     for (let c of this.words[randomIndex]) {
-      this.selectedWord = this.selectedWord + 'x';
+      this.hiddenWord = this.hiddenWord + 'x';
     }
   }
 
